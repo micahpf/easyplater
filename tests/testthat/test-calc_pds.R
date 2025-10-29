e6a0b73a5558aae03e5c9a0c869ba4b31d5e8b32
micahpf <micahpfletcher@gmail.com>
2025-10-29 10:33:38 +0000

    Test calc_row_column_score()

--- tests/testthat/test-calc_pds.R
+++ tests/testthat/test-calc_pds.R
@@ -10,14 +10,28 @@ test_that("sas edge range calculation works", {
   expect_equal(calc_sas_edge_range(33L, 10, 8), 86)
 })
 
-test_that("calc_pds_global calculation works when given valid input", {
+test_that("calc_pds_global() calculation works when given valid input", {
   expect_equal(
-    calc_pds_global(example_plate_df, names(example_plate_df)[2:5], c(5, 5, 10, 4), c(86:95)),
-    20.1845486)
+    calc_pds_global(example_plate_df,
+                    names(example_plate_df)[2:5],
+                    c(5, 5, 10, 4),
+                    c(86:95)),
+    expected = 20.1845486)
 })
 
 test_that("calc_pds_global() errors when given non-96-well plate", {
   expect_error(
-    calc_pds_global(example_plate_df[49:96,], names(example_plate_df)[2:5], c(5, 5, 10, 4), c(86:95)),
+    calc_pds_global(example_plate_df[49:96,],
+                    names(example_plate_df)[2:5],
+                    c(5, 5, 10, 4),
+                    c(86:95)),
     class = "simpleError")
+})
+
+test_that("calc_row_column_score() calculation works", {
+  expect_equal(
+    calc_row_column_score(example_plate_df,
+                    names(example_plate_df)[2:5],
+                    c(5, 5, 10, 4)),
+    expected = 470)
 })
